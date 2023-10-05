6cc947a65e81a2c251d3f89c02b5fef40228b42b
naufalshiddiq014 <naufalshiddiq014@gmail.com>
2023-10-05 15:03:58 +0700

    first

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.cerbung',
+  appName: 'cerbung',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
